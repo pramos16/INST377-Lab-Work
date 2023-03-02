b720bae2b09dd78c22c597ee58b4b8df9cb4c985
pramos16 <72025503+pramos16@users.noreply.github.com>
2023-03-02 13:20:54 -0500

    Final Lab 5 Commit

--- client/lab_5/script.js
+++ client/lab_5/script.js
@@ -5,7 +5,11 @@
 
 /* A quick filter that will return something based on a matching input */
 function filterList(list, query) {
-  /*
+  return list.filter((item)=> {
+    const lowerCaseName = item.name.toLowerCase();
+    const lowerCaseQuery = query.toLowerCase();
+    return lowerCaseName.includes(lowerCaseQuery);
+  })  /*
     Using the .filter array method, 
     return a list that is filtered by comparing the item name in lower case
     to the query in lower case
@@ -17,7 +21,7 @@ function filterList(list, query) {
 async function mainEvent() { // the async keyword means we can make API requests
   const mainForm = document.querySelector('.main_form'); // This class name needs to be set on your form before you can listen for an event on it
   // Add a querySelector that targets your filter button here
-
+  const filterButton = document.querySelector('.filter_button')
   let currentList = []; // this is "scoped" to the main event function
   
   /* We need to listen to an "event" to have something happen in our page - here we're listening for a "submit" */
@@ -55,6 +59,17 @@ async function mainEvent() { // the async keyword means we can make API requests
   });
 
 
+  filterButton.addEventListener('click', (event) => {
+    console.log('clicked FilterButton');
+
+    const formData = new FormData(mainForm);
+    const formProps = Object.fromEntries(formData);
+
+    console.log(formProps);
+    const newList = filterList(currentList, formProps.resto);
+
+    console.log(newList);
+})
   /*
     Now that you HAVE a list loaded, write an event listener set to your filter button
     it should use the 'new FormData(target-form)' method to read the contents of your main form
